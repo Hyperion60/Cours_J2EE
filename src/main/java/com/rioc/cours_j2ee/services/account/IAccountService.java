c77317ec996650c49eb515db82d32b9e48d70b8a
Hyperion <35610696+Hyperion60@users.noreply.github.com>
2022-11-14 18:10:25 +0100

    Ajout des fonctions de récupérations et de modification des comptes

--- src/main/java/com/rioc/cours_j2ee/services/account/IAccountService.java
+++ src/main/java/com/rioc/cours_j2ee/services/account/IAccountService.java
@@ -2,6 +2,13 @@ package com.rioc.cours_j2ee.services.account;
 
 import com.rioc.cours_j2ee.models.dao.Account;
 
+import java.util.List;
+
 public interface IAccountService {
-    public Account postAccount(Account account) throws Exception;
+    Account postAccount(Account account) throws Exception;
+    Account patchAccount(Account account) throws Exception;
+    Account patchAccount(Account account, boolean creation) throws Exception;
+    List<Account> getAccounts();
+
+    Account getAccount(Integer pk) throws Exception;
 }
