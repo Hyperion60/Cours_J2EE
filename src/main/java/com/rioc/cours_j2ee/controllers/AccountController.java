9280da265a6dacb65f67aa4f7de27c1d185229b7
Hyperion <35610696+Hyperion60@users.noreply.github.com>
2022-11-14 18:08:50 +0100

    Ajout des requêtes PATCH, GET, DELETE pour /accounts + GET pour /account/{pk}

--- src/main/java/com/rioc/cours_j2ee/controllers/AccountController.java
+++ src/main/java/com/rioc/cours_j2ee/controllers/AccountController.java
@@ -8,6 +8,8 @@ import com.rioc.cours_j2ee.services.account.IAccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
 public class AccountController {
@@ -32,5 +34,46 @@ public class AccountController {
         }
 
         return new Gson().toJson(account);
+    }
+
+    @PatchMapping("/accounts")
+    public String updateAccounts(
+            @RequestBody List<Account> accounts
+    ) {
+        for (Account account:accounts) {
+            try {
+                service.patchAccount(account, true);
+            } catch (Exception e) {
+                throw new ApiException(e.getMessage(), HttpStatus.NOT_ACCEPTABLE);
+            }
+        }
+        return "Update success";
+    }
+
+    @GetMapping("/accounts")
+    public String getAccounts() {
+        StringBuilder output = new StringBuilder("{'accounts': [");
+        for (Account account: service.getAccounts()) {
+            output.append(new Gson().toJson(account));
+            output.append(",");
+        }
+        output.deleteCharAt(output.length() - 1);
+        output.append("]}");
+        return output.toString();
+    }
+
+    @DeleteMapping("/accounts")
+    public String delAccounts() {
+        throw new ApiException("Method not allowed", HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    @GetMapping("/account/{pk}")
+    public String getAccount(
+            @PathVariable("pk") Integer pk) {
+        try {
+            return new Gson().toJson(service.getAccount(pk));
+        } catch (Exception e) {
+            throw new ApiException(e.getMessage(), HttpStatus.NOT_ACCEPTABLE);
+        }
     }
 }
